d3610469121c5596b170f58d864ff27df54afd89
find-my-way <1109164856@qq.com>
2024-10-29 18:08:19 +0800

    Site updated: 2024-10-29 18:08:18

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/27/STM32-ADC-Application-Note-1/","2024/10/28/马克思主义基本原理/"];function toRandomPost(){
+var posts=["2024/10/27/STM32-ADC-Application-Note-1/","2024/10/29/STM32-FreeRtos-V1-应用手册1/","2024/10/28/马克思主义基本原理/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
